d5094353dcfcac8f69bda76534d758726df39258
Bunncakess <kenjiobena@gmail.com>
2024-02-29 12:09:32 -0600

    Update sketch.js
    FATTY

--- A1 Character/sketch.js
+++ A1 Character/sketch.js
@@ -8,9 +8,15 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES)
 }
 
 function draw() {
   background(220);
   circle(width/2, height/2, 100)
+  rect(200,490,300,150) //torso
+  rect(270,640,30,175) //left leg
+  rect(390,640,30,175) //left leg
+  
 }
+
