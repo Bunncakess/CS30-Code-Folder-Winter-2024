6552606de3547636d9bb80f1b98ebee213311779
Bunncakess <kenjiobena@gmail.com>
2024-03-05 12:03:33 -0600

    MAN IN BLACK
    Drawing with objects

--- A1 Character/sketch.js
+++ A1 Character/sketch.js
@@ -12,11 +12,27 @@ function setup() {
 }
 
 function draw() {
-  background(220);
-  circle(width/2, height/2, 100)
-  rect(200,490,300,150) //torso
-  rect(270,640,30,175) //left leg
-  rect(390,640,30,175) //left leg
-  
+  background("green");
+  translate(window/2, height/2);
+    fill(92, 64, 51)
+  circle(width/2, height/2, 100);
+    fill(0)
+  rect(200,490,300,150); //torso
+    fill(0,0,255);
+  rect(270,640,30,175); //left leg
+   fill(0, 0, 255);
+  rect(390,640,30,175); //right leg
+    fill(92, 64, 51);
+  rect(500,550,60,10); //right arm
+    fill(92, 64, 51);
+  rect(140,550,60,10); //left arm
+    
+    fill(255);
+  circle(width/2.1, height/2, 15); //right eye
+  circle(width/1.9, height/2, 15); //left eye
+  rect(width/2.1,460 ,20,3)
+
+  rect(270,790,30,25)
+  rect(390,790,30,25)
 }
 
