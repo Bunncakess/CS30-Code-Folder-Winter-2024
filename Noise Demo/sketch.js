eee15538dadddebf63878d72ab2bd8e288b6a00e
Bunncakess <kenjiobena@gmail.com>
2024-03-08 11:24:52 -0600

    TIME NOISE

---
+++ Noise Demo/sketch.js
@@ -0,0 +1,24 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+let x, y;
+let time = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  x = width/2;
+  y = height/2;
+}
+
+function draw() {
+  background(220);
+  x = noise(time) * width;
+
+  fill("black");
+  circle(x,y,40);
+
+  time=0.01;
+}
