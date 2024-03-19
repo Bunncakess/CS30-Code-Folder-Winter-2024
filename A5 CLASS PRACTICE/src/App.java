7e0c53d5a41a6bcd00d4311b7346a082326fa7b7
Bunncakess <kenjiobena@gmail.com>
2024-03-19 12:11:41 -0600

    PRACTICE

---
+++ A5 CLASS PRACTICE/src/App.java
@@ -0,0 +1,16 @@
+// Program/Assignment name: Practice Assignment 5
+
+// Student name: Kenji
+// Date: 3/19/24
+
+// Program Description: Paragraph
+
+public class App{
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to Computer Science 30\r\n" +
+                        "My Name is Kenji and my favourite food is dog and animals.\r\n" +
+                        "Java has all sorta of weird \"quirks\" which make displaying text a bit more challenging.\r\n" + 
+                        "Sometimes characters like parentheses (sometimes called brackets) don't work correctly.\r\n" + 
+                        "Escape sequences require us to use a backslash '\\' in order to output these special characters.");
+    }
+}
---
+++ Java 2/Java 2/src/App.java
@@ -0,0 +1,21 @@
+Program/Assignment name: (The name you gave the program)
+
+Student name: (Your name)
+
+Date: (The date you completed the program)
+
+Program Description:
+
+ 
+
+
+
+
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
