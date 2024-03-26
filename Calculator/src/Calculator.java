5d0b443ff4119dfc584d610fd170cb5ce809bb94
Bunncakess <kenjiobena@gmail.com>
2024-03-26 12:08:57 -0600

    CHICKENS

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,8 @@
+public class Calculator {
+    public static void main(String[] args) throws Exception {
+        double Chicken = 0.75;
+        double Money = 4.75;
+        double Calc = Money/Chicken;
+        System.out.println("You can buy this amount of chickens:" + Calc);
+    }
+}
